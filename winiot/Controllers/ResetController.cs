0bca3f6e9766dc03be3422d567c5ce5495cb1a15
Djonatas Tenfen <djonatas.tenfen@gmail.com>
2015-10-16 03:30:20 -0300

    correcoes finais

---
+++ winiot/Controllers/ResetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace winiot.Controllers
+{
+    public class ResetController : ApiController
+    {
+        // GET: api/Lampada
+        public void Get()
+        {
+            WebApiApplication.LampadaStatus_Count = 0;
+            WebApiApplication.LampadaStatus = 0;
+            WebApiApplication.SireneStatus_Count = 0;
+            WebApiApplication.SireneStatus = 0;
+            WebApiApplication.TomadaStatus_Count = 0;
+            WebApiApplication.TomadaStatus = 0;
+        }
+    }
+}
